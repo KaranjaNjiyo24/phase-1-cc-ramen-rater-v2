fc0ef0c5544cc79c83964939fd42dfa3804b8d3f
Andrew Karanja Njiyo <karanjaandrew2000@outlook.com>
2024-10-15 01:48:49 +0300

    Adds a submit listener but images disappeared need to debug

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 // index.js
 
+import { e } from "vitest/dist/reporters-rzC174PQ.js";
+
 // Callbacks
 const handleClick = (ramen) => {
   // Add code
@@ -19,10 +21,42 @@ const handleClick = (ramen) => {
     commentOnFood.textContent = ramen.comment;
 };
 
-// const addSubmitListener = () => {
-//   // Add code
+const addSubmitListener = () => {
+  // Add code
+  form = document.getElementById('new-ramen');
+  form.addEventListener('submit', function(e) {
+    e.preventDefault() //avoid refreshing
+    //getting values from form we assign the elements first
+    const name = document.getElementById('new-name')
+    const restaurant = document.getElementById('new-restaurant')
+    const image = document.getElementById('new-image')
+    const rating = document.getElementById('new-rating')
+    const comment = document.getElementById('new-comment')
 
-// }
+    const newRamen = {
+      name: name.value,
+      restaurant: restaurant.value,
+      image: image.value,
+      rating: rating.value,
+      comment: comment.value
+    }
+
+    const img = document.createElement('img')
+    img.src = newRamen.image
+    img.alt = newRamen.name
+
+    img.addEventListener('click', () => {
+      handleClick(newRamen)
+    })
+
+    const ramenMenu = document.getElementById('ramen-menu')
+    ramenMenu.appendChild(img)
+
+    form.reset()
+  })
+
+
+}
 
 const displayRamens = () => {
   // Add code
@@ -45,6 +79,7 @@ const main = () => {
   // Invoke displayRamens here
   // Invoke addSubmitListener here
   displayRamens()
+  addSubmitListener()
 }
 
 main()
@@ -53,7 +88,7 @@ main()
 // Export functions for testing
 export {
   displayRamens,
-  // addSubmitListener,
-  // handleClick,
+  addSubmitListener,
+  handleClick,
   main,
 };
