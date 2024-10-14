ec1717aba2b4c6bd30873c881b98c17bbc73ecbe
Andrew Karanja Njiyo <karanjaandrew2000@outlook.com>
2024-10-15 00:58:49 +0300

    See and display all ramen images

--- src/index.js
+++ src/index.js
@@ -1,29 +1,42 @@
-// index.js
+// // index.js
 
-// Callbacks
-const handleClick = (ramen) => {
-  // Add code
-};
+// // Callbacks
+// const handleClick = (ramen) => {
+//   // Add code
+// };
 
-const addSubmitListener = () => {
-  // Add code
-}
+// const addSubmitListener = () => {
+//   // Add code
+
+// }
 
 const displayRamens = () => {
   // Add code
+  fetch('http://localhost:3000/ramens') 
+  .then(response => response.json())
+  .then(ramens => {
+    const ramenMenuDiv = document.getElementById('ramen-menu');
+    ramens.forEach(ramen => {
+      const img = document.createElement('img');
+      img.src = ramen.image
+      ramenMenuDiv.appendChild(img);
+    });
+  });
 };
 
 const main = () => {
   // Invoke displayRamens here
   // Invoke addSubmitListener here
+  displayRamens()
 }
 
 main()
 
+
 // Export functions for testing
 export {
   displayRamens,
-  addSubmitListener,
-  handleClick,
+  // addSubmitListener,
+  // handleClick,
   main,
 };
